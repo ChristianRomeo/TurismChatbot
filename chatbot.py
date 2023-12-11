80e985b53ee2516e51159bd5f3a4f684f44a8344
Christian Romeo <christian.romeo3@gmail.com>
2023-12-11 20:04:10 +0100

    llamaindex test

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,17 @@
 import os
+import time
+import huggingface_hub
 import openai
 import pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from datasets import load_dataset
 from transformers import GPT2TokenizerFast
 from langchain.text_splitter import CharacterTextSplitter
+#from optimum.nvidia import LlamaForCausalLM
+import g4f
+from langchain_g4f import G4FLLM
+from llama_index.llms import HuggingFaceLLM
+from g4f import Provider, models
 
 PINECONE_KEY = os.environ.get('PINECONE_KEY')
 if PINECONE_KEY is None:
@@ -16,51 +23,17 @@ HUGGINGFACE_API_KEY = os.environ.get('HUGGINGFACE_API_KEY')
 if HUGGINGFACE_API_KEY is None:
     raise ValueError("Hugging Face key not found. Please set the HUGGINGFACE_API_KEY environment variable.")
 
-index_name = "texst1"
-pinecone.init(api_key=PINECONE_KEY, environment="gcp-starter")
-model_name = 'text-embedding-ada-002'
-
-system_prompt = (
-    "This is a knowledgeable Tourism Assistant designed to provide visitors with "
-    "information, recommendations, and tips for exploring and enjoying their destination. "
-    "The assistant is familiar with a wide range of topics including historical sites, "
-    "cultural events, local cuisine, accommodations, transportation options, and hidden gems. "
-    "It offers up-to-date and personalized information to help tourists make the most of their trip."
+response = g4f.ChatCompletion.create(
+    model=g4f.models.gpt_4,
+    messages=[{"role": "user", "content": "what is the weather in nice?"}],
 )
+print(response)
 
-# Load a small test dataset from Hugging Face Hub
-dataset = load_dataset("jayantdocplix/falcon-small-test-dataset")
+#remove from the response the first 100 characted but only the first time
 
-# Convert the 'train' split of the dataset to a pandas DataFrame and take the first 50 rows
-df = dataset['train'].to_pandas().head(50) # type: ignore
 
-# Initialize a character-based text splitter
-text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(GPT2TokenizerFast.from_pretrained("gpt2-xl"), chunk_overlap=50)
-#overlap: how many characters will be duplicated at the end of one chunk and the start of the next chunk. 
-#           Helpful because it can prevent the model from missing or misinterpreting the context that might occur at the boundaries of chunks
-
-# Apply the text splitter to each row of text in the DataFrame
-df['text'] = df['text'].apply(text_splitter.split_text)
-
-# Flatten the list of lists of text chunks into a single list
-flattened_texts = [text for sublist in df['text'].tolist() for text in sublist]
-
-# Embed the flattened texts using OpenAI embeddings
-embed = OpenAIEmbeddings().embed_documents(flattened_texts)
-
-index = pinecone.GRPCIndex(index_name)
-
-items_to_upsert = [
-   {
-    # Each item to upsert contains a unique ID, the embedding values, and associated metadata
-       "id": str(index),
-       "values": embedding,
-       "metadata": {"text": text}
-   }
-   for index, (embedding, text) in enumerate(zip(embed, df['text'].tolist()))
-]
-
-index.upsert(items_to_upsert)
+def chunker(seq, batch_size):
+    return (seq[pos:pos + batch_size] for pos in range(0, len(seq), batch_size))
 
 def query_pinecone(question, top_k=5):
        # Generate the embedding for the question
@@ -95,10 +68,85 @@ def generate_response(context, question):
          return "I'm sorry, I couldn't generate a response."
 
 
+index_name = "chatbot"
+model_name = 'text-embedding-ada-002'
+system_prompt = (
+    "This is a knowledgeable Tourism Assistant designed to provide visitors with "
+    "information, recommendations, and tips for exploring and enjoying their destination. "
+    "The assistant is familiar with a wide range of topics including historical sites, "
+    "cultural events, local cuisine, accommodations, transportation options, and hidden gems. "
+    "It offers up-to-date and personalized information to help tourists make the most of their trip."
+)
+
+pinecone.init(api_key=PINECONE_KEY, environment="gcp-starter")
+pinecone.delete_index(index_name)
+
+if index_name not in pinecone.list_indexes():
+    # we create a new index
+    pinecone.create_index(
+        name=index_name,
+        metric='cosine',
+        dimension=1536,  # 1536 dim of text-embedding-ada-002
+        #metadata_config={'indexed': ['wiki-id', 'title']}
+    )
+# wait for index to be initialized
+#while not pinecone.describe_index(index_name).status['ready']: # type: ignore
+    time.sleep(1)
+
+#tokenizer = AutoTokenizer.from_pretrained("text-embedding-ada-002", padding_side="left")
+#model = LlamaForCausalLM.from_pretrained("text-embedding-ada-002", use_fp8=True)
+#model_inputs = tokenizer(flattened_texts, return_tensors="pt").to("cuda")
+#embed = model.generate(**model_inputs, top_k=40, top_p=0.7, repetition_penalty=10)
+
+
+# Load a small test dataset from Hugging Face Hub
+dataset = load_dataset("jayantdocplix/falcon-small-test-dataset")
+
+# Convert the 'train' split of the dataset to a pandas DataFrame and take the first 50 rows
+df = dataset['train'].to_pandas().head(50) # type: ignore
+
+# Initialize a character-based text splitter
+text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(GPT2TokenizerFast.from_pretrained("gpt2-xl"), chunk_overlap=50)
+#overlap: how many characters will be duplicated at the end of one chunk and the start of the next chunk. 
+#           Helpful because it can prevent the model from missing or misinterpreting the context that might occur at the boundaries of chunks
+
+# Apply the text splitter to each row of text in the DataFrame
+df['text'] = df['text'].apply(text_splitter.split_text)
+
+# Flatten the list of lists of text chunks into a single list
+flattened_texts = [text for sublist in df['text'].tolist() for text in sublist]
+
+# Embed the flattened texts using OpenAI embeddings
+embed = OpenAIEmbeddings().embed_documents(flattened_texts)
+
+index = pinecone.GRPCIndex(index_name)
+time.sleep(1)
+
+items_to_upsert = [
+   {
+    # Each item to upsert contains a unique ID, the embedding values, and associated metadata
+       "id": str(index),
+       "values": embedding,
+       "metadata": {"text": text}
+   }
+   for index, (embedding, text) in enumerate(zip(embed, df['text'].tolist()))
+]
+
+#index.upsert(items_to_upsert, async_req=True)
+# Chunk the items to upsert
+chunks = list(chunker(items_to_upsert, batch_size=100))
+
+# Upsert the chunks asynchronously
+async_results = [index.upsert(vectors=chunk, async_req=True) for chunk in chunks]
+
+# Wait for the operations to complete and retrieve the responses
+[async_result.result() for async_result in async_results] # type: ignore
+
 while True:
     # Read a line of input from the user
     question = input("Please enter your question: ")
-
+    if question == "exit":
+            break
     # Query Pinecone
     context = query_pinecone(question)
 
@@ -115,4 +163,3 @@ print("Complete")
 # 1. What is the best time to visit Paris?
 #LLAMAINDEX for alternatives for report in case
 # data processing
-
--- test.py
+++ test.py
@@ -1,204 +1,84 @@
-'''
-DOCUMENT_SOURCE_DIRECTORY = r"C:\Users\chris\OneDrive\Desktop\EIT\2°Year\project\data"
-# Target root files directory to for documents to load
-loader = DirectoryLoader(r"C:\Users\chris\OneDrive\Desktop\EIT\2°Year\project\data", glob='*')
-# Load documents
-documents = loader.load()
-# Split documents into chunks before creating embeddings
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, length_function=len)
-docs = text_splitter.split_documents(documents)
+import glob
+import os
+import shutil
+import time
+import g4f
+from g4f import Provider, models
+from langchain.llms.base import LLM
+from langchain_g4f import G4FLLM
+import pandas as pd
+import dask.dataframe as dd
+from llama_index import PromptTemplate, VectorStoreIndex, ServiceContext, Document, StorageContext, load_index_from_storage
+from llama_index.llms import OpenAI
+import pinecone
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext
+from llama_index.vector_stores import PineconeVectorStore
+import torch
+from llama_index.llms import HuggingFaceLLM
 
-endpoint_url = "https://data.odeuropa.eu/sparql"
-query = """
-SELECT *
-WHERE {
-  ?s ?p ?o.
-}
-LIMIT 100
-"""
+system_prompt = (
+    "This is a knowledgeable Tourism Assistant designed to provide visitors with "
+    "information, recommendations, and tips for exploring and enjoying their destination. "
+    "The assistant is familiar with a wide range of topics including historical sites, "
+    "cultural events, local cuisine, accommodations, transportation options, and hidden gems. "
+    "It offers up-to-date and personalized information to help tourists make the most of their trip."
+)
 
-def get_results(endpoint_url, query):
-    sparql = SPARQLWrapper(endpoint_url)
-    query = endpoint_url + "?" + urlencode({"query": query})
-    sparql.setQuery(query)
-    sparql.setReturnFormat(XML)
-    return sparql.query().convert()
+service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo-1106", temperature=0.6, system_prompt=system_prompt))
 
-results = get_results(endpoint_url, query)
+# ask the user if he wants to delete the storage and compute it again
+print("Do you want to delete the storage and compute it again? (y/n)")
+if input()== "y" and os.path.exists("./storage"):
+    shutil.rmtree("./storage")
 
+PINECONE_KEY = os.environ.get('PINECONE_KEY')
+index_name = "chatbot"
 
-data = results["results"]["bindings"]
-# Assuming that the data contains 'subject', 'predicate', and 'object'.
-
-df = pd.DataFrame(columns=["subject", "predicate", "object"])
-for row in data:
-    df = df.append({
-        "subject": row["subject"]["value"],
-        "predicate": row["predicate"]["value"],
-        "object": row["object"]["value"]
-    }, ignore_index=True)
-
-index_name = 'odeuropa'
-vector_dimension = 1536
-# Check if the index exists, if not, create one
-if index_name not in pinecone.list_indexes():
-    pinecone.create_index(index_name, dimension=vector_dimension)
-
-# Connect to the index
-index = pinecone.Index(index_name)
-
-
-def get_embedding(text):
-    response = openai.Embedding.create(
-        input=text,
-        model="text-similarity-babbage-001",  # For example, can use any available model
-    )
-    return response["data"][0]["embedding"]
-
-# Example: Generate embeddings for a series texts (assume `df["object"]` contains text)
-embeddings = df["object"].apply(lambda x: get_embedding(x))
-
-# Next, you'd upsert these embeddings into Pinecone. You'll want to pair each vector with a unique ID.
-for idx, embedding in enumerate(embeddings):
-    index.upsert(items=[(str(idx), embedding)])
-
-def chatbot_response(user_input):
-    embedding = get_embedding(user_input)
-    query_results = index.query(queries=[embedding], top_k=1)
-    most_relevant_id = query_results["matches"][0]["id"]
-    most_relevant_text = df.loc[int(most_relevant_id), "object"]
-    
-    response = openai.Completion.create(
-        prompt=f"Based on the following knowledge: {most_relevant_text}. How would you respond to: {user_input}?",
-        model="gpt-3.5-turbo",  # Assuming using OpenAI's GPT-3.5 model. Replace with appropriate model ID
-        max_tokens=150
-    )
-    return response.choices[0].text.strip()
-
-# Example usage:
-user_message = "Tell me about historical odors in Europe."
-print(chatbot_response(user_message))
-
-sparql = SPARQLWrapper("https://data.odeuropa.eu/sparql")
-sparql.setQuery("""
-SELECT *
-WHERE {
-    ?s ?p ?o .
-}
-LIMIT 100
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-
-print (pinecone_datasets.list_datasets())
-
-dataset = pinecone_datasets.load_dataset('wikipedia-simple-text-embedding-ada-002')
-# we drop sparse_values as they are not needed for this example
-dataset.documents.drop(['sparse_values', 'metadata'], axis=1, inplace=True)
-dataset.documents.rename(columns={'blob': 'metadata'}, inplace=True)
-# we will use rows of the dataset up to index 30_000
-dataset.documents.drop(dataset.documents.index[30_000:], inplace=True)
-
-index_name = 'chatbot-onboarding'
-
+pinecone.init(api_key=PINECONE_KEY, environment="gcp-starter")
 if index_name not in pinecone.list_indexes():
     # we create a new index
     pinecone.create_index(
         name=index_name,
         metric='cosine',
         dimension=1536,  # 1536 dim of text-embedding-ada-002
-        metadata_config={'indexed': ['wiki-id', 'title']}
+        #metadata_config={'indexed': ['wiki-id', 'title']}
     )
-    # wait a moment for the index to be fully initialized
-    time.sleep(1)
-
-index = pinecone.GRPCIndex(index_name)
-# wait a moment for the index to be fully initialized
+else:
+    pinecone.delete_index(index_name)
+# wait for index to be initialized
+#while not pinecone.describe_index(index_name).status['ready']: # type: ignore
 time.sleep(1)
+    
 
-index.describe_index_stats()
+if not os.path.exists("./storage"):
+    documents = []
 
-index.upsert_from_dataframe(dataset.documents, batch_size=100)
+    # Get a list of all CSV files in the directory
+    csv_files = glob.glob('./data/*.csv')
 
-tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
-model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
-
-def get_embedding(text):
-    inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True)
-    outputs = model(**inputs)
-    embeddings = outputs.last_hidden_state.mean(dim=1)
-    return embeddings.detach().numpy()
-
-query_text = "What is machine learning?"
-query_embedding = get_embedding(query_text)
-
-# Query Pinecone for similar vectors
-top_k = 5  # number of closest vectors to return
-results = index.query(queries=[query_embedding], top_k=top_k)
-
-# 'results' now contains the ids and distances of the top_k closest vectors to the query_embedding
-for result in results.results:
-    print(f"ID: {result.id}, Distance: {result.distance}")
-
-# Initialize the model
-model = g4f.models.gpt_4  # or another model you want to use
-
-# Create a chat completion
-response = g4f.ChatCompletion.create(
-    model=model,
-    messages=[{"role": "user", "content": query_text}],
-)
-
-# 'response' now contains the model's response to the query_text
-print(response)
-
-index_name = 'odeon-europa'
-model_name = 'text-embedding-davinci-002'
-embed = OpenAIEmbeddings(
-    document_model_name=model_name,
-    query_model_name=model_name,
-    openai_api_key=OPENAI_API_KEY
-)
-embed_dimension = 1536
-
-if index_name not in pinecone.list_indexes():
-    pinecone.create_index(
-        name=index_name,
-        metric='cosine',
-        dimension=embed_dimension
+    storage_context = StorageContext.from_defaults(
+        vector_store=PineconeVectorStore(pinecone.Index(index_name))
     )
-index = pinecone.Index(index_name)
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=400,
-    chunk_overlap=20,
-    length_function=tiktoken_len,
-    separators=["\n\n", "\n", " ", ""]
-)
+    for file in csv_files:
+        #df = dd.read_csv(file, dtype=str, encoding="mac_roman")
+        # Load the CSV file into a DataFrame
+        chunksize = 100000 # Adjust this value based on your system's capabilities
+        for chunk in pd.read_csv(file, dtype=str, encoding="mac_roman", parse_dates=True, chunksize=chunksize):
+            print("hjello")
+            # Convert the DataFrame into a list of Document objects
+            docs = [Document(doc_id=str(i), text=row.to_string()) for i, row in chunk.iterrows()]
+            # Add the documents to the list
+            documents.extend(docs)
 
-batch_limit = 100
-texts = []
-metadatas = []
+    index = VectorStoreIndex.from_documents(documents, service_context=service_context, storage_context=storage_context, show_progress=True)
+    print("hello")
+    storage_context.persist(persist_dir=f"./storage")
+    print("hwello")
 
-# Fetch and process your data here. The following is a placeholder.
-data = [{'id': 1, 'url': 'https://data.odeuropa.eu/sparql', 'title': 'Sample Title', 'content': 'Sample Content'}]
+else:
+    index = load_index_from_storage(StorageContext.from_defaults(persist_dir=f"./storage"), service_context=service_context)
 
+chat_engine = index.as_chat_engine(verbose=True)
 
-for i, record in enumerate(tqdm(data)):
-    metadata = {
-        'item_uuid': str(record['id']),
-        'source': record['url'],
-        'title': record['title']
-    }
-    record_texts = text_splitter.split_text(record['content'])
-    record_metadatas = [{ "chunk": j, "text": text, **metadata } for j, text in enumerate(record_texts)]
-    texts.extend(record_texts)
-    metadatas.extend(record_metadatas)
-
-    if len(texts) >= batch_limit:
-        ids = [str(uuid4()) for _ in range(len(texts))]
-        embeds = embed.embed_documents(texts)
-        index.upsert(vectors=zip(ids, embeds, metadatas))
-        texts = []
-        metadatas = []
-'''
+print(chat_engine.chat(input("Please enter your question: ")).response)
